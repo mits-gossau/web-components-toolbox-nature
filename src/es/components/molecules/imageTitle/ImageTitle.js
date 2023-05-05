fdf569e41044d11d7d5fe5b1e33621c05aa8e099
Laurin Graf <Laurin.Graf@mgb.ch>
2023-05-05 11:13:38 +0200

    imageTitle update

--- src/es/components/molecules/imageTitle/ImageTitle.js
+++ src/es/components/molecules/imageTitle/ImageTitle.js
@@ -4,6 +4,11 @@ import { Shadow } from '../../web-components-toolbox/src/es/components/prototype
 
 /**
  * @export
+ * @class ImageTitle
+ * @attribute {
+ *  {background-color} [#color] define the background color of div.title
+ *  {color} [#color] define the color of div.text
+ * }
  * @type {CustomElementConstructor}
  */
 export default class ImageTitle extends Shadow() {
@@ -24,10 +29,11 @@ export default class ImageTitle extends Shadow() {
     this.css = /* css */`
       :host {
         display: grid;
+        --any-display: grid;
         grid-template-columns: 50% 50%;
       }
       :host > div {
-        padding: calc(var(--content-spacing) * 2) calc(var(--content-spacing) * 4);
+        padding: var(--padding, calc(var(--content-spacing) * 2) calc(var(--content-spacing) * 4));
       }
       :host > div > * {
         position: relative;
@@ -46,29 +52,28 @@ export default class ImageTitle extends Shadow() {
       }
       @media only screen and (max-width: _max-width_) {
         :host {
-            grid-template-columns: unset;
+          grid-template-columns: unset;
         }
         :host > div {
-            padding: var(--content-spacing-mobile);
+          padding: var(--padding-mobile, var(--content-spacing-mobile));
         }
         :host a-emotion-pictures {
-            grid-row: 2;
-            grid-column-end: 1;
+          grid-row: 2;
+          grid-column-end: 1;
         }
       }
     `
     /** @type {import("../../web-components-toolbox/src/es/components/prototypes/Shadow.js").fetchCSSParams[]} */
     const styles = [
       {
-          path: `${import.meta.url.replace(/(.*\/)(.*)$/, '$1')}../../web-components-toolbox/src/css/reset.css`, // no variables for this reason no namespace
-          namespace: false
+        path: `${import.meta.url.replace(/(.*\/)(.*)$/, '$1')}../../web-components-toolbox/src/css/reset.css`, // no variables for this reason no namespace
+        namespace: false
       },
       {
-          path: `${import.meta.url.replace(/(.*\/)(.*)$/, '$1')}../../web-components-toolbox/src/css/style.css`, // apply namespace and fallback to allow overwriting on deeper level
-          namespaceFallback: true
+        path: `${import.meta.url.replace(/(.*\/)(.*)$/, '$1')}../../web-components-toolbox/src/css/style.css`, // apply namespace and fallback to allow overwriting on deeper level
+        namespaceFallback: true
       }
     ]
     return this.fetchCSS(styles)
   }
-
 }
