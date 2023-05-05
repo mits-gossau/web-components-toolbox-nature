5154d5079aa32e24247eff82b6b6e6920f03b6c4
Weedshaker <weedshaker@gmail.com>
2023-05-05 08:37:18 +0200

    case sensitive renaming link to the example fix of fetchCss, which does not need then

--- src/es/components/molecules/imageTitle/ImageTitle.js
+++ src/es/components/molecules/imageTitle/ImageTitle.js
@@ -7,10 +7,6 @@ import { Shadow } from '../../web-components-toolbox/src/es/components/prototype
  * @type {CustomElementConstructor}
  */
 export default class ImageTitle extends Shadow() {
-  constructor (...args) {
-    super(...args)
-  }
-
   connectedCallback () {
     if (this.shouldRenderCSS()) this.renderCSS()
   }
@@ -63,16 +59,16 @@ export default class ImageTitle extends Shadow() {
     `
     /** @type {import("../../web-components-toolbox/src/es/components/prototypes/Shadow.js").fetchCSSParams[]} */
     const styles = [
-        {
-            path: `${import.meta.url.replace(/(.*\/)(.*)$/, '$1')}../../web-components-toolbox/src/css/reset.css`, // no variables for this reason no namespace
-            namespace: false
-        },
-        {
-            path: `${import.meta.url.replace(/(.*\/)(.*)$/, '$1')}../../web-components-toolbox/src/css/style.css`, // apply namespace and fallback to allow overwriting on deeper level
-            namespaceFallback: true
-        }
-        ]
-        return this.fetchCSS(styles, false).then(() => this.calcColumnWidth())
+      {
+          path: `${import.meta.url.replace(/(.*\/)(.*)$/, '$1')}../../web-components-toolbox/src/css/reset.css`, // no variables for this reason no namespace
+          namespace: false
+      },
+      {
+          path: `${import.meta.url.replace(/(.*\/)(.*)$/, '$1')}../../web-components-toolbox/src/css/style.css`, // apply namespace and fallback to allow overwriting on deeper level
+          namespaceFallback: true
+      }
+    ]
+    return this.fetchCSS(styles)
   }
 
 }
